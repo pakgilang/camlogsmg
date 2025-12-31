f4ebc50c148d23325e30da7b414ea260249f62b0
pakgilang <74170689+pakgilang@users.noreply.github.com>
2025-12-31 10:53:01 +0700

    Update sw.js

--- smg-pwa/site/sw.js
+++ smg-pwa/site/sw.js
@@ -1,6 +1,6 @@
 /* sw.js — SMG PWA (lightweight) */
 
-const CACHE = "smg-pwa-v2";
+const CACHE = "smg-pwa-v2.1";
 
 // Static assets yang aman dicache
 const ASSETS = [
@@ -63,4 +63,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
